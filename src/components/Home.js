d2571b8c33fdc103bb7c9397c4fa8eb4c3977cef
Xin Zhang <xinjohnzhang@gmail.com>
2019-08-24 19:12:46 -0700

    Create starting components

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Home extends Component {
+  render () {
+    return (
+      <div className='home-container'>
+        <header>james jean</header>
+        <main>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Home;
