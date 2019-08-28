e9e5679dd68746954e8c95eb3de2f34f711db130
Xin Zhang <xinjohnzhang@gmail.com>
2019-08-28 16:25:11 -0700

    Add helper to Router to restore scroll when path change

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 import Home from './Home';
 import Gallery from './Gallery';
 
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Home} />
-      <Route path="/gallery" component={Gallery} />
+      <ScrollToTop>
+        <Route exact path="/" component={Home} />
+        <Route path="/gallery" component={Gallery} />
+      </ScrollToTop>
     </Router>
   );
 }
