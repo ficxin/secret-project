3fca59bdc662e10d357fe9708a52a843a35e292c
Xin Zhang <xinjohnzhang@gmail.com>
2019-08-28 09:14:33 -0700

    Setup React Router Dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './Home';
+import Gallery from './Gallery';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route path="/gallery" component={Gallery} />
+    </Router>
   );
 }
 
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Link } from "react-router-dom";
+import Gallery from './Gallery';
 
 class Carousel extends Component {
   state = {
@@ -19,7 +21,7 @@ class Carousel extends Component {
   componentDidUpdate(prevProp, prevState) {
     if (!prevState.positions) {
       // Styles are not applied until current callstack is completed and delay is required to read correct styles.
-      setTimeout(() => this.getSlidePositions(), 0)
+      setTimeout(() => this.getSlidePositions(), 100)
     }
   }
 
@@ -81,7 +83,7 @@ class Carousel extends Component {
             {index ?
               <div>
                 <a
-                  className="nav-button prev-slide"
+                  className='nav-button prev-slide'
                   onClick={() => this.prevSlide()}>
                     <span>&#9001;</span>
                 </a>
@@ -89,27 +91,32 @@ class Carousel extends Component {
             : null}
             <div>
               <a 
-                className="nav-button next-slide"
+                className='nav-button next-slide'
                 onClick={() => this.nextSlide()}>
                   <span>&#9002;</span>
               </a>
             </div>
           </div>
           <ul 
-            className="carousel"
+            className='carousel'
             style={transition}>
-            {artists.map(({ name, thumbnail_url } ) => (
-              <li key={name} className='slide'>
-                <img
-                  className='thumbnail'
-                  src={thumbnail_url}
-                  alt={name}
-                  onClick={() => true}
-                />
-              </li>
-            ))}
+            {artists.map(({ name, thumbnail_url }) => {
+              const path = name.replace(/\s/g, '').toLowerCase();
+              return (
+                <li key={name} className='slide'>
+                  <Link to={`/gallery/${path}`}>
+                    <img
+                      className='thumbnail'
+                      src={thumbnail_url}
+                      alt={name}
+                    />
+                  </Link>
+                </li>
+              )}
+            )}
           </ul>
         </section>
+        <Route path='/gallery/:id' component={Gallery} />
       </div>
     );
   }
